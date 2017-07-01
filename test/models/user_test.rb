4bc2c1ae5e42c8761d317be5c343ebbb3b5872b7
wild7 <wild7_life25@yahoo.co.jp>
2017-07-02 02:16:04 +0900

    6.2.3項 長さを検証する

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,17 +5,27 @@ class UserTest < ActiveSupport::TestCase
     @user = User.new(name: "Example User", email: "user@example.com")
   end
 
-  test 'shoud be valid' do
+  test 'should be valid' do
     assert @user.valid?
   end
 
-  test 'name shoud be present' do
+  test 'name should be present' do
     @user.name = ""
     assert_not @user.valid?
   end
 
-  test 'email shoud be present' do
+  test 'name should not be too long' do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test 'email should be present' do
     @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test 'email should not be too long' do
+    @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
 end
