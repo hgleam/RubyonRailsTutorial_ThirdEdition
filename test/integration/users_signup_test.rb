5d32bce0a62e6be8ef22e92990858fa9703c9c15
wild7 <wild7_life25@yahoo.co.jp>
2017-07-06 23:16:30 +0900

    演習2実施

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,13 +4,23 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test 'invalid signup information' do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, user: { name:                   "",
-                               email:                  "user@invalid",
-                               password:               "foo",
-                               passoword_confirmation: "bar"
-                             }
+      post users_path, user: { name:                  "",
+                               email:                 "user@invalid",
+                               password:              "foo",
+                               password_confirmation: "bar"
+                         }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert-danger', 'The form contains 4 errors'
+    assert_select 'div.field_with_errors', 8
+    assert_select 'div#error_explanation' do
+      assert_select 'li', 'Name can\'t be blank'
+      assert_select 'li', 'Email is invalid'
+      assert_select 'li', 'Password confirmation doesn\'t match Password'
+      assert_select 'li', 'Password is too short (minimum is 6 characters)'
+    end
+
   end
 
   test 'valid signup information' do
