0eff7f5d3fe978a4459a64fdce6de1dd3d633c89
wild7 <wild7_life25@yahoo.co.jp>
2017-07-08 08:52:22 +0900

    8.2.2項 現在のユーザー

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,4 +3,10 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # 現在ログインしているユーザーを返す(ユーザーがログイン中の場合のみ)
+  def current_user
+    @current_user ||= User.find_by(session[:user_id])
+  end
+
 end
