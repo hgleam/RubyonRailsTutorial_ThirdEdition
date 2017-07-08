4374159e8ab5639ed04216d06bf70b70b77501ff
wild7 <wild7_life25@yahoo.co.jp>
2017-07-08 11:27:35 +0900

    8.4.3項 ユーザーを忘れる

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,14 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+
   # ユーザーを永続的セッションに記憶する
   def remember(user)
     user.remember
@@ -31,6 +39,7 @@ module SessionsHelper
 
   # 現在のユーザーをログアウトする
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
