479ab261839595e7dd2e95c94372ef80dd612fb8
wild7 <wild7_life25@yahoo.co.jp>
2017-07-08 11:22:30 +0900

    8.4.2項 ログイン状態の保持

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,12 @@ class User < ActiveRecord::Base
   # 永続的セッションで使用するユーザーをデータベースに記憶する
   def remember
     self.remember_token = User.new_token
-    update.attribute(:remember_digest, User.digest(remember_token))
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
 end
