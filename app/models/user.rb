8e9d941ed3f99b95f7d0aa0a9be49594665dfd3e
wild7 <wild7_life25@yahoo.co.jp>
2017-07-08 18:19:08 +0900

    演習1実施 2パタン目

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,16 +9,18 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
-  # 与えられた文字列のハッシュ値を返す
-  def self.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine::cost
-    BCrypt::Password.create(string, cost: cost)
-  end
-
-  # ランダムなトークンを返す
-  def self.new_token
-    SecureRandom.urlsafe_base64
+  class << self
+    # 与えられた文字列のハッシュ値を返す
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine::cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+  
+    # ランダムなトークンを返す
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 
   # 永続的セッションで使用するユーザーをデータベースに記憶する
@@ -37,5 +39,4 @@ class User < ActiveRecord::Base
   def forget
     update_attribute(:remember_digest, nil)
   end
-
 end
