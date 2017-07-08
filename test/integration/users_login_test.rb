4c831a229bb2e58e2fe145dbe12e06975aeed5cb
wild7 <wild7_life25@yahoo.co.jp>
2017-07-08 10:06:17 +0900

    8.2.5項 ユーザー登録時にログイン

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -4,6 +4,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
   end
+  
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -25,5 +26,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", user_path(@user)
   end
 
-
+  test 'valid signup information' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path user: { name:                  "Example User",
+                                           email:                 "user@example.com",
+                                           password:              "password",
+                                           password_confirmation: "password"}
+    end
+    assert_template 'users/show'
+    assert is_logged_in?
+  end
 end
