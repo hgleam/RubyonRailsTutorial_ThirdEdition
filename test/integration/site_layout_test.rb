21422a7daf15d863d31e236cd481f4618462fb38
wild7 <wild7_life25@yahoo.co.jp>
2017-07-09 09:44:25 +0900

    演習2実施

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,12 +1,28 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
   test 'layout_links' do
+    # 未ログイン
     get root_path
     assert_template "static_pages/home"
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", 'http://www.railstutorial.org/'
+    assert_select "a[href=?]", signup_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+
+    # ログイン済み
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
   end
 end
