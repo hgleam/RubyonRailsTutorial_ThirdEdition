38938d3c506049fd2b993fc37dd295135b670677
wild7 <wild7_life25@yahoo.co.jp>
2017-07-15 15:48:42 +0900

    11.1.2章 Micropostのバリデーション

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+    # 次の行は慣習的に間違っている
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test 'should be valid' do
+    assert @micropost.valid?
+  end
+
+  test 'user id shoud be present' do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  test 'content should be presence' do
+    @micropost.content = nil
+    assert_not @micropost.valid?
+  end
+
+  test 'content should be at most 140 characters' do
+    @micropost.content = "a" * 141
+    assert_not @micropost.valid?
+  end
 end
